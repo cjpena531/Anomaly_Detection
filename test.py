cc2144248562a953feeae90d8d2f5f416b409829
cjpena531 <cataspena@hotmail.com>
2023-01-25 11:42:35 -0800

    Delete Test files

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib as mplt
-import numpy as np
-import math
-from sklearn.metrics import classification_report
-import xgboost
-
-print('DONE')
